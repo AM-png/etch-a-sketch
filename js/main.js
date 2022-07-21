f451b9770362cfd43a9240194b2499f3f8819e2a
arbaaz <mirzaarbaaz123@gmail.com>
2022-07-20 21:30:54 -0400

    finish assignment

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let container = document.querySelector('.container')
+
+let button = document.createElement('button')
+button.textContent = 'Click for prompt'
+document.querySelector('body').insertBefore(button, container)
+document.querySelector('body').style.cssText = "display:flex; flex-direction: column; align-items: center; margin: 20px"
+
+document.querySelector('button').addEventListener('click', () => {
+    button = prompt("input numbre of squares from 1 - 100")
+    for (let i = 1; i <= button && i < 101; i++) {
+        let column = document.createElement('div')
+        for (let j = 1; j <= button && j < 101; j++) {
+            let row = document.createElement('div')
+            row.style.cssText = "background: honeydew; height: 15px; width: 15px"
+            container.style.cssText = "margin: 20px auto; display:flex; flex-wrap: wrap; border: 1px solid black"
+            column.appendChild(row)
+        }
+        container.appendChild(column)
+    }
+    document.querySelectorAll('div').forEach(item => {
+        item.addEventListener('mouseover', () => {
+            item.style.backgroundColor = "coral"
+        })
+    })
+})
